ff2b6bf6f287a4bb92b768816f66965585aa0f1a
Byeonggon Lee <gonny952@gmail.com>
2020-03-13 18:02:56 +0900

    Solved bad horse

---
+++ codejam/bad-horse/solve.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+def bfs(graph, visited, queue, xset, yset):
+  while queue:
+    e = queue.pop(0)
+    if e not in visited:
+      if e not in xset and e not in yset:
+        xset.add(e)
+      if e in xset:
+        if len(set(graph[e]).intersection(xset)) >= 1:
+          return False
+        else:
+          yset.update(graph[e])
+      elif e in yset:
+        if len(set(graph[e]).intersection(yset)) >= 1:
+          return False
+        else:
+          xset.update(graph[e])
+      visited.add(e)
+      queue.extend(graph[e])
+  return True
+
+def main():
+  ncases = int(input())
+  for i in range(ncases):
+    graph = {}
+    visited = set()
+    queue = []
+    xset = set()
+    yset = set()
+    npairs = int(input())
+    for j in range(npairs):
+      a, b = input().strip().split()
+      if a not in graph:
+        graph[a] = []
+      if b not in graph:
+        graph[b] = []
+      graph[a].append(b)
+      graph[b].append(a)
+    ret = True
+    for e in graph:
+      queue.append(e)
+      ret = bfs(graph, visited, queue, xset, yset)
+      if ret == False:
+        break
+    print(f'Case #{i + 1}: ' + ('Yes' if ret else 'No'))
+main()
